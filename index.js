1d3af7f850e8bbfa4963ea961d9f9857db74f434
Kade Fraser <kadejfraser@gmail.com>
2023-05-20 12:08:02 -0700

    Caught up with sample assignment and fixed their bugs

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 
 const setup = () => {
-  $(".card").on(("click"), function(){
+  let firstCard = undefined;
+  let secondCard = undefined;
+  $(".card").on(("click"), function() {
+    if($(this).hasClass("flip")){
+      return;
+    }
+    console.log("clicked");
     $(this).toggleClass("flip");
+
+    if (!firstCard){
+      firstCard = $(this).find(".front_face")[0];
+    } else {
+      secondCard = $(this).find(".front_face")[0];
+      console.log(firstCard, secondCard);
+
+      if(firstCard.src == secondCard.src){
+        console.log("match");
+        $(`#${firstCard.id}`).parent().off("click");
+        $(`#${secondCard.id}`).parent().off("click");
+        console.log($(`#${firstCard.id}`));
+        console.log($(`#${secondCard.id}`));
+      firstCard = undefined;
+        secondCard = undefined;  
+      }else{
+        console.log("no match");
+        flipBack(firstCard);
+        flipBack(secondCard);
+        firstCard = undefined;
+        secondCard = undefined;
+      }
+    }
+
+
+
   });
 }
+
+function flipBack(card){
+  setTimeout(() => {
+    $(`#${card.id}`).parent().toggleClass("flip");
+  }, 1000);
+}
+
 
 $(document).ready(setup);
